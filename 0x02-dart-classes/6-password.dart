10c07a2dade45551fccc0f76d1296e2fb8f995c9
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-07-21 05:09:21 +0100

    6

---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,36 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    if (_password == null) return false; // Add null check
+
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    if (!_containsUppercase() || !_containsLowercase() || !_containsNumbers()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool _containsUppercase() {
+    return _password.contains(RegExp(r'[A-Z]'));
+  }
+
+  bool _containsLowercase() {
+    return _password.contains(RegExp(r'[a-z]'));
+  }
+
+  bool _containsNumbers() {
+    return _password.contains(RegExp(r'[0-9]'));
+  }
+
+  @override
+  String toString() {
+    return "Password: ${isValid()}"; // Call the isValid() method here
+  }
+}
