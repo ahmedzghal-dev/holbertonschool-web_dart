5136a168d71b62295cb054d140bcee66d9dcd19a
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-07-21 05:14:58 +0100

    6

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,78 +1,48 @@
-class Password {
-  String _password;
+import '6-password.dart';
 
-  Password({required String password}) : _password = password;
+class User extends Password {
+  String? name;
+  int? age;
+  double? height;
+  int? id;
+  String user_password = '';
 
-  bool isValid() {
-    if (_password == null) return false; // Add null check
-
-    if (_password.length < 8 || _password.length > 16) {
-      return false;
-    }
-
-    if (!_containsUppercase() || !_containsLowercase() || !_containsNumbers()) {
-      return false;
-    }
-
-    return true;
-  }
-
-  bool _containsUppercase() {
-    return _password.contains(RegExp(r'[A-Z]'));
-  }
-
-  bool _containsLowercase() {
-    return _password.contains(RegExp(r'[a-z]'));
-  }
-
-  bool _containsNumbers() {
-    return _password.contains(RegExp(r'[0-9]'));
-  }
-
-  @override
-  String toString() {
-    return "Password: ${isValid()}"; // Call the isValid() method here
-  }
-}
-
-class User {
-  int id;
-  String name;
-  int age;
-  double height;
-  String? user_password; // Nullable password property
-  Password password; // Instance of the Password class
-
-  User({
-    required this.id,
-    required this.name,
-    required this.age,
-    required this.height,
-    required String user_password,
-  }) : password = Password(password: user_password);
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-      'height': height,
-      'user_password': user_password,
-    };
+  User(
+      {id = int,
+      name = String,
+      age = int,
+      height = double,
+      user_password = String})
+      : super(password: user_password) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.user_password = user_password;
   }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-      user_password: userJson['user_password'],
-    );
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height'],
+        user_password: '');
+  }
+
+  Map toJson() {
+    return {
+      'id': this.id,
+      'name': this.name,
+      'age': this.age,
+      'height': this.height
+    };
   }
 
   @override
   String toString() {
-    return "User(id : $id ,name: $name, age: $age, height: $height, ${password.toString()})";
+    final pass = new Password(password: this.user_password);
+
+    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password: ${pass.isValid()})';
   }
 }
--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,79 +1,33 @@
 class Password {
-  String _password;
+  String _password = '';
 
-  Password({required String password}) : _password = password;
+  Password({password = String}) {
+    this._password = password;
+  }
+
+  String get password {
+    return this._password;
+  }
+
+  void set password(String password) {
+    this._password = password;
+  }
 
   bool isValid() {
-    if (_password == null) return false; // Add null check
-
-    if (_password.length < 8 || _password.length > 16) {
-      return false;
+    if (this._password.length >= 8 && this._password.length <= 16) {
+      if (this._password.contains(RegExp(r'[A-Z]')) &&
+          this._password.contains(RegExp(r'[a-z]'))) {
+        if (this._password.contains(RegExp(r'[0-9]'))) {
+          return true;
+        }
+      }
     }
 
-    if (!_containsUppercase() || !_containsLowercase() || !_containsNumbers()) {
-      return false;
-    }
-
-    return true;
-  }
-
-  bool _containsUppercase() {
-    return _password.contains(RegExp(r'[A-Z]'));
-  }
-
-  bool _containsLowercase() {
-    return _password.contains(RegExp(r'[a-z]'));
-  }
-
-  bool _containsNumbers() {
-    return _password.contains(RegExp(r'[0-9]'));
+    return false;
   }
 
   @override
   String toString() {
-    return "Your Password is: $_password"; // Call the isValid() method here
-  }
-}
-
-class User {
-  int id;
-  String name;
-  int age;
-  double height;
-  String? user_password; // Nullable password property
-  Password? password; // Nullable instance of the Password class
-
-  User({
-    required this.id,
-    required this.name,
-    required this.age,
-    required this.height,
-    required String? user_password,
-  }) : password =
-            user_password != null ? Password(password: user_password) : null;
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-      'height': height,
-      'user_password': user_password,
-    };
-  }
-
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-      user_password: userJson['user_password'],
-    );
-  }
-
-  @override
-  String toString() {
-    return "User(id : $id ,name: $name, age: $age, height: $height, ${password?.toString() ?? 'Password: false'})";
+    return 'Your Password is: ${this._password}';
   }
 }
